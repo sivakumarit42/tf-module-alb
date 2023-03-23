11175d6d624da26033bbe953c6fb7a1ae4e73326
Sivakumar S <sivakumarit42@gmail.com>
2023-03-23 18:25:07 +0530

    changes

---
+++ vars.tf
@@ -0,0 +1,10 @@
+variable "env" {}
+variable "tags" {}
+
+variable "name" {}
+variable "internal" {}
+variable "load_balancer_type" {}
+variable "enable_deletion_protection" {
+  default = false
+}
+variable "subnets" {}
